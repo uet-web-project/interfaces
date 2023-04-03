25fa6cc14d526412f2cc88ffd7e0f268927af562
Trần Chiến Thắng <ctb0k33@gmail.com>
2023-04-03 12:50:38 +0700

    add car interface

---
+++ car.interface.ts
@@ -0,0 +1,19 @@
+export interface ICar {
+  _id?: string;
+  registrationNumber: string;
+  registrationDate: string;
+  registrationLocation: string;
+  licensePlate: string;
+  vehicleOwner: string;
+  manufacturer: string;
+  model: string;
+  version: string;
+  purpose: string;
+  width: number;
+  height: number;
+  groundClearance: number;
+  cylinderCapacity: number;
+  torque: number;
+  gearbox: number;
+  registrationCenter: string;
+}
